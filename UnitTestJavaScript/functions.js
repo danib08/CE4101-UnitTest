488a50a745c0f6d1781a903f8ca02773b0908eff
BadStone3100 <48070861+Oscarmendez825@users.noreply.github.com>
2022-09-19 22:01:57 -0600

    JavaScript Unit Tests completed

--- UnitTestJavaScript/functions.js
+++ UnitTestJavaScript/functions.js
@@ -1,4 +1,20 @@
+const axios = require('axios');
+
+/***
+ * Functions that will be tested
+ */
 const functions = {
     add: (num1,num2) => num1 + num2,
+
+    fetchUser: () => 
+    axios
+    .get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error'),
+
+    execFirst: () => console.log('Executing in First Place...'),
+    execLast: () => console.log('Executing in Last Place...'),
 }
+    
+
 module.exports = functions;
--- UnitTestJavaScript/functions.test.js
+++ UnitTestJavaScript/functions.test.js
@@ -1,5 +1,34 @@
+const { execFirst, execLast } = require('./functions');
 const functions = require('./functions')
 
+
+/**
+ * Execute this function before all the tests
+ */
+beforeAll(() => execFirst());
+
+
+/**
+ * Execute this function after all the tests
+ */
+afterAll(() => execLast());
+
+
+/***
+ * Test add funtion
+ * Using comparatives functions
+ */
 test('Add 2 + 2 to not equal 4', () => {
     expect(functions.add(2,2)).not.toBe(5);
+});
+
+/***
+ * Test fetchUser function
+ * Using async functions
+ */
+test('Waiting for the user name to be Leanne Grahan', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+
 });
